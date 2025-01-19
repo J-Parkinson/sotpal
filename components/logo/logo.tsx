7e36e792c35b2007086d7720a639f979c85e858b
Jack Parkinson <jrp43213@gmail.com>
2025-01-19 15:34:59 +0000

    Create Logo and MainLogo components

---
+++ components/logo/logo.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import logo from "./logo.module.css";
+import AnimationWrapper from "@/components/styles/animationWrapper";
+
+const Logo = ({ text }: { text: string }) => {
+    return <h1 className={logo.Logo}>{text}</h1>;
+};
+
+export const MainLogo = () => <AnimationWrapper>
+    <div className={logo.MainLogo}>
+        <Logo text="SOME OF THESE"/><Logo text="PEOPLE ARE LYING"/>
+    </div>
+</AnimationWrapper>;
+
+export default Logo;
