9598997c6388c31d387bb0e483ce553bb6eda28f
Jack Parkinson <jrp43213@gmail.com>
2025-01-19 11:46:18 +0000

    Update metadata to match nature of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-starter.vercel.app'),
-  title: 'Vercel Postgres Demo',
+  metadataBase: new URL('https://sotpal.vercel.app'),
+  title: 'Some of these People are Lying',
   description:
-    'A simple Next.js app with Vercel Postgres as the database',
+    'A website that implements the "Technical Difficulties" game "Two of these People Are Lying", with support for more people.',
 }
 
 const inter = Inter({
