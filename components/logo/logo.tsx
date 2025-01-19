3b8252fc6cf60b068af88d5ab4d4dd066985bd3c
Jack Parkinson <jrp43213@gmail.com>
2025-01-19 19:18:18 +0000

    Rename animation wrapper

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import logo from "./logo.module.css";
-import AnimationWrapper from "@/components/styles/animationWrapper";
+import AnimationWrapper from "@/components/styles/animation-wrapper";
 
 const Logo = ({ text }: { text: string }) => {
     return <h1 className={logo.Logo}>{text}</h1>;
