f2662aeeac9005a0429746dc8303f0c7b9793a76
Jack Parkinson <jrp43213@gmail.com>
2025-01-19 19:18:34 +0000

    Create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import Logo, {MainLogo} from "@/components/logo/logo";
+'use client'
+
+import {MainLogo} from "@/components/logo/logo";
+import React from "react";
+import Button from "@/components/button/button";
 
 export const runtime = 'edge'
 export const preferredRegion = 'home'
@@ -6,8 +10,12 @@ export const dynamic = 'force-dynamic'
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
+    <main className="relative flex min-h-screen flex-col items-center justify-center gap-10">
       <MainLogo/>
+      <div className="p-5 flex flex-col items-center justify-center gap-5">
+        <Button onClick={() => {}}>Start Game</Button>
+          <Button onClick={() => {}}>Join Game</Button>
+      </div>
     </main>
   )
 }
